ce2a79ed8d1fa462bc196a421fc92c58e0c43255
Ben-Mustion <benmustion@gmail.com>
2019-08-30 19:57:01 -0700

    Back end setup

---
+++ src/index.js
@@ -0,0 +1,39 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+dotenv.config();
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('err', (err) => {
+  console.log('Error connecting to mongo instance', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
